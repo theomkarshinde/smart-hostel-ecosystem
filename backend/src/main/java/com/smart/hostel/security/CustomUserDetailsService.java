4b7b6eef31255a8725c73b70d31420f21c627f6c
theomkarshinde <142740945+theomkarshinde@users.noreply.github.com>
2026-02-07 00:07:33 +0530

    Update CustomUserDetailsService.java

--- backend/src/main/java/com/smart/hostel/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/smart/hostel/security/CustomUserDetailsService.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +28,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 				.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
 		String roleName = user.getRole().getRoleName();
-		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPasswordHash(),
+		return new User(user.getUsername(), user.getPasswordHash(),
 				Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + roleName)));
 	}
 }
